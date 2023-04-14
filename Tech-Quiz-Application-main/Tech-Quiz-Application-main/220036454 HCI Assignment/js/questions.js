5ff7f4f5658e9848164f24ab68b6e8ecc56cdc61
Godwinnartey <130666194+Godwinnartey@users.noreply.github.com>
2023-04-14 08:02:03 +0000

    Add files via upload

---
+++ Tech-Quiz-Application-main/Tech-Quiz-Application-main/220036454 HCI Assignment/js/questions.js
@@ -0,0 +1,125 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which team won the 2018 UCL?",
+    answer: "Real Madrid",
+    options: [
+      "Barcelona",
+      "Liverpool",
+      "Bayern",
+      "Real Madrid"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Ghana is popularly known for exporting?",
+    answer: "Gold",
+    options: [
+      "Copper",
+      "Cocoa",
+      "Gold",
+      "Timber"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What region is Bank of Ghana located?",
+    answer: "Greater Accra Region",
+    options: [
+      "Ashanti Region",
+      "Greater Accra Region",
+      "Western Region",
+      "Northern Region"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+    {
+    numb: 6,
+    question: "Javascript is an _________ language?",
+    answer: "Object-Oriented",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above",
+    ]
+  },
+  {
+    numb: 7,
+    question: "What does DBMS stands for?",
+    answer: "Database Management System",
+    options: [
+      "Database Management System",
+      "Database System",
+      "Database Manipulating System",
+      "Database Managerial System",
+    ]
+  },
+  {
+    numb: 8,
+    question: "The German team lost against the Belgium team in what year?",
+    answer: "2023",
+    options: [
+      "1896",
+      "2006",
+      "2023",
+      "2018",
+    ]
+  },
+  {
+    numb: 9,
+    question: "How can a datatype be declared to be a constant type?",
+    answer: "Const",
+    options: [
+      "Const",
+      "var",
+      "let",
+      "constant",
+    ]
+  },
+  {
+    numb: 10,
+    question: "What network operators has more users?",
+    answer: "MTN",
+    options: [
+      "MTN",
+      "Tigo",
+      "Vodafone",
+      "Glo",
+    ]
+  },
+  {
+    numb: 11,
+    question: "Ghanaians elect thier president every _____________ years?",
+    answer: "4",
+    options: [
+      "6",
+      "4",
+      "3",
+      "5",
+    ]
+  }
+];
